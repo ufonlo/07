26c58f886dc32842fd70abae067d8a1fb0a1bb8b
ufonlo <socufo@gmail.com>
2018-05-07 12:45:17 +0300

    start

---
+++ src/one/firstHomework.java
@@ -0,0 +1,16 @@
+package one;
+import java.util.Scanner;
+
+public class firstHomework {
+	public static void main (String []args) {
+		System.out.print("Enter day of week");
+		Scanner weekDay = new Scanner(System.in);
+		int number = weekDay.nextInt();
+		if (number <= 5 && number >0) {
+			System.out.print("weekday");
+		}else {
+			System.out.print("weekend");
+		}
+		weekDay.close();
+	}
+}
