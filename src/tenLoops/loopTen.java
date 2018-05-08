bb46d26a75bd181d9a2af2befd46dd834079d851
ufonlo <socufo@gmail.com>
2018-05-08 14:55:00 +0300

    Conditions Start

--- src/tenLoops/loopTen.java
+++ src/tenLoops/loopTen.java
@@ -1,5 +1,15 @@
 package tenLoops;
-
+import java.util.Scanner;
 public class loopTen {
-
+	public static void main (String []args) {
+		System.out.print("Enter day of week");
+		Scanner weekDay = new Scanner(System.in);
+		int number = weekDay.nextInt();
+		if (number <= 5 && number >0) {
+			System.out.print("weekday");
+		}else {
+			System.out.print("weekend");
+		}
+		weekDay.close();
+	}	
 }
