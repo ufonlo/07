8dc651de84a7d1c7a95d4dab6a5aad87ac3b765e
ufonlo <socufo@gmail.com>
2018-05-11 17:57:45 +0300

    +2-true

--- src/fiveConditions/BmiInfo.java
+++ src/fiveConditions/BmiInfo.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 
 public class BmiInfo {
 	public static void main(String[] args) {
-
+		double weight,height,bmi;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your weight");
-		double weight  = scan.nextDouble();
-		
+		weight  = scan.nextDouble();
+		System.out.println(weight);
 		System.out.println("Enter your height");
-		double height = scan.nextDouble();
+		height = scan.nextDouble();
+		System.out.println(height);
+		
 		
 		String errorMesage = "Error";
-		double bmi = ((weight / height)*(weight / height));
+		bmi = (weight / (height*height));
+		
+		
 		String mesageUnderweight = "Underweight";
 		String mesageNormal = "Normal";
 		String mesageOverweight = "Overweight";
-		// create output values
 
-		
-		if (weight<=0|height<=0) {
+	
+		if (weight<=0||height<=0) {
 			System.out.print(errorMesage);
 			
 		} else if (bmi <= 18.5) {
-			System.out.print(mesageUnderweight);
+			System.out.print(mesageUnderweight + "  BMI = " + bmi);
 		} else if (18.5 <= bmi||bmi <= 25.0) {
-			System.out.print(mesageNormal);
+			System.out.print(mesageNormal + "  BMI = " + bmi);
 		} else {
-			System.out.print(mesageOverweight);
+			System.out.print(mesageOverweight + "  BMI = " + bmi);
 		}
 
 		// create BL
